66a0e2f4c4f4da9b093412a60386ca0e11c99e70
Matteo <matteop18@gmail.com>
2020-01-14 18:43:02 +0100

    controllo se numero dell'utente è fra i numeri generati dal pc

--- js/script.js
+++ js/script.js
@@ -1,56 +1,49 @@
-// // Il computer deve generare 16 numeri casuali da 1 a 100.
+// Campo Minato
+// Il computer deve generare 16 numeri casuali da 1 a 100.
 
-var numeriRandomPc = [];
+// creare array vuoto per inserire i numeri geneati dal pc
+var numberArray = [];
 
-for (var i = 1; i <= 16; i++) {
-  var numeriRandom = generaNumeroRandom(1, 100);
-  console.log(numeriRandom);
-  numeriRandomPc.push(numeriRandom);
+while (numberArray.length < 16) {
+  var numberPc = randomNumber(1, 100);
+  if (numberArray.includes(numberPc) == false) {
+    numberArray.push(numberPc);
+  }
 }
-
-console.log(numeriRandomPc);
-
-function generaNumeroRandom(min, max) {
+console.log(numberArray);
+function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+// console.log(randomNumber(1, 100));
 
-// // In seguito deve chiedere all’utente di inserire per 84 volte un numero da 1 a 100, se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti continua chiedendo all’utente un altro numero.
-//
-var numeriUtenti = [];
-//
-for (var i = 1; i < 5; i++) {
-  var utente = parseInt(prompt('Inserisci un numero da 1 a 84'));
-  numeriUtenti.push(utente);
+// In seguito deve chiedere all’utente di inserire per 84 volte un numero da 1 a 100, se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti continua chiedendo all’utente un altro numero.
+
+// creare una funziona che controlli che il numero inserito dall'utente  sia dentro all'array con dentro i numeri generati dal pc
+
+function checkArrayNumber(array, number) {
+  for (var i = 0; i < array.length; i++) {
+    if (number == array[i]) {
+      return true;
+    }
   }
-console.log(numeriUtenti);
-
-
-
-
-
-
-
-// var i = 0;
-// var posizione = false;
+  return false;
+}
+// // dichiarare una variabile con i tentativi massimi che l'utente può utilizzare
 //
-// while (i < numeriRandom.length && posizione == false) {
-  //   if (numeriUtenti[i] == numeriRandom) {
-    //     posizione = true;
-    //   }
-    //   i++
-    // }
-    // console.log(posizione);
+var maxTries = 84;
+//
+// // creare un array vuoto dove inserire i tentativi dell'utente
+//
+var tries = [];
+//
+// // dichiarare una variabile che contenga il punteggio dell'utente
+//
+var point = 0;
 
-
-
-
-
-// La partita termina quando il giocatore inserisce un numero “vietato”, ovvero presente nella lista di numeri random, o raggiunge il numero massimo possibile di tentativi consentiti.
-// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
-// BONUS: all’inizio il software richiede anche una difficoltà all’utente
-// che cambia il range di numeri casuali.
-// Con difficoltà 0 => numeri vietati da 1 a 100, tentativi 84
-// con difficoltà 1 => numeri vietati  da 1 a 80, tentativi 64
-// con difficoltà 2 => numeri vietati  da 1 a 50, tentativi 34
-// Come fatto assieme analizzate prima cosa deve fare il software, poi scriviamo i passaggi in italiano, solo dopo convertiamo in sintassi js aiutandoci con le slide e con la documenazione ufficiale.
-// Proviamo ad inserire delle piccole funzioni, ogni funzione deve svolgere un compito preciso e restiturci qualcosa.
+ while (tries.length < maxTries && checkArrayNumber(numberArray, triesUser) == false) {
+    var triesUser = parseInt(prompt('Scrivi un numero da 1 a 100!'));
+   // controllo che sia una bomba
+   if (checkArrayNumber(numberArray, triesUser) == true) {
+     console.log('Hai perso');
+   }
+ }
